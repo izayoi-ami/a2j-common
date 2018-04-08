5aca931b11f60b3bab2a17194c2e1925548d470d
Helsa Chan <helsachan@gmail.com>
2018-04-08 12:26:20 +0800

    add brians summary

--- src/pages/summary.vue
+++ src/pages/summary.vue
@@ -1,12 +1,18 @@
 <template>
   <q-page padding>
-    <p>Summary</p>
+    <h3 class="caption">Summary</h3>
     <p>Date of accident: {{ this.$store.state.date.dateOfAccident }} </p>
     <p>Age: {{ this.$store.state.profile.age }} </p>
     <p>Gender: {{ this.$store.state.profile.gender }} </p>
-    <p>Pre-accident monthly income: {{ this.$store.state.profile.preAccidentIncome }} </p>
+    <p>Ability to socialize after accident: {{this.$store.state.data.lossOfSociety}}</p>
+    <p>Pre-accident monthly income: i{{ this.$store.state.profile.preAccidentIncome }} </p>
     <p>Expected retirement age: {{ this.$store.state.profile.ageOfRetirement }}  </p>
     <p>Incurred medical expenses {{ this.$store.state.profile.expenses }} </p>
+    <p>Medical Report Received? {{this.$store.state.data.medicalReportExist}}</p>
+    <div v-if="medicalReportExist">
+      <p class="caption">Estimated loss of earning capacity: {{this.$store.state.data.earningCapacity}}</p>
+    </div>
+    <q-btn @click="$router.push('result')" color="white" text-color="black" label="Result" />
   </q-page>
 </template>
 
